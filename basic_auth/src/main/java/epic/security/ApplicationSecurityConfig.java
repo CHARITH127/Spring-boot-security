ac4f367318bc31871f9e3c8e6d4f2e9b22d78c48
CHARITH127 <charithrullz993@gmail.com>
2023-03-21 16:10:17 +0530

    add basic auth

---
+++ basic_auth/src/main/java/epic/security/ApplicationSecurityConfig.java
@@ -0,0 +1,25 @@
+package epic.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import sun.misc.Contended;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+
+}
