b5dda37b7372ae945e97751d10567dc4f206b54e
CHARITH127 <charithrullz993@gmail.com>
2023-03-21 13:51:13 +0530

    create spring boot security demo

---
+++ test_api/src/main/java/lk/epic/WebApplicationInitializer.java
@@ -0,0 +1,12 @@
+package lk.epic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplicationInitializer.class);
+    }
+}
